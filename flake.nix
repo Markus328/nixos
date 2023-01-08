389bd82e586b8dcb06897f72d799483007d9ed49
Markus328 <markus328@tutanota.com>
2023-01-08 17:02:37 -0300

    Updated

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nur }: {
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
