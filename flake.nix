818b3da6555e9c942050cd667263f2235fb7c2ed
Markus328 <markus328@tutanota.com>
2023-01-10 21:50:31 -0300

    fixed some things

--- flake.nix
+++ flake.nix
@@ -1,14 +1,8 @@
 {
-
-  inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
-  };
-  outputs = { self, nixpkgs, hyprland }: {
-    # replace 'joes-desktop' with your hostname here.
+  outputs = { self, nixpkgs, }: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        hyprland.nixosModules.default
         ./configuration.nix
       ];
     };
